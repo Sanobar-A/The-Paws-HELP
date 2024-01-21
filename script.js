84276334625c606cdb42a47581ea48b11528d165
Sanobar-A <asanobar3@gmail.com>
2024-01-21 22:20:18 +0530

    added some html pages

--- script.js
+++ script.js
@@ -5,4 +5,46 @@ function login(){
 function home(){
     window.open("index.html");
     window.close("login.html");
+    window.close("hospital.html");
+    window.close("shelter.html");
+    window.close("rescue.html");
+    window.close("supplies.html");
+    window.close("legal.html");
+    window.close("online_vet.html");
+}
+function hospitals(){
+    window.open("hospital.html");
+    window.close("home.html");
+}
+function shelter(){
+    window.open("shelter.html");
+    window.close("home.html");
+}
+function rescue(){
+    window.open("rescue.html");
+    window.close("home.html");
+}
+function supplies(){
+    window.open("supplies.html");
+    window.close("home.html");
+}
+function legal(){
+    window.open("legal.html");
+    window.close("home.html");
+}
+function online_vet(){
+    window.open("online_vet.html");
+    window.close("home.html");
+}
+function phospitals(){
+    window.open("phospitals.html");
+    window.close("home.html");
+}
+function ghospitals(){
+    window.open("ghospitals.html");
+    window.close("home.html");
+}
+function about(){
+    window.open("about.html");
+    window.close("home.html");
 }
