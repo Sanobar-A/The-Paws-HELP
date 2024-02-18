86d0ad21e0a45a0de452115509bcd2eee03c2ae9
Sanobar-A <asanobar3@gmail.com>
2024-02-18 17:27:17 +0530

    added feedback form and connected to mysql

---
+++ feedback.php
@@ -0,0 +1,31 @@
+<?php
+	$email = $_POST['email'];
+	$field = $_POST['field'];
+	$feedback = $_POST['feedback'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','feedback');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into feedbackdetails(email, field, feedback) values(?, ?, ?)");
+		
+		if ($stmt === false) {
+			die("Error in prepare: " . $conn->error);
+		}
+		
+		$stmt->bind_param("sss", $email, $field, $feedback);
+		
+		$execval = $stmt->execute();
+		
+		if($execval === false) {
+			die("Error during execution: " . $stmt->error);
+		}
+		
+		echo "successfull...";
+		
+		$stmt->close();
+		$conn->close();
+	}
+?>
